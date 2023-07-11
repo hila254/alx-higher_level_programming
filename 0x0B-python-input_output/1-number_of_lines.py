a02442611c513ae1b8e005cfd6bd9bca6d92b210
Hillary Gor <125491337+hila254@users.noreply.github.com>
2023-07-12 01:11:51 +0300

    Update 1-number_of_lines.py

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
-"""Defines a text file line-counting function."""
+"""Defines a function to count the number of lines in a text file."""
 
 def number_of_lines(filename=""):
     """Return the number of lines in a text file."""
     lines = 0
-    with open(filename) as f:
-        for line in f:
+    with open(filename, 'r') as file:
+        for line in file:
             lines += 1
     return lines
